93b9b5322d2116f23a6c3353e6da102ee2720637
mbd <doganmbdogan@gmail.com>
2024-10-01 09:15:40 +0300

    doctordata map

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import {doctorData} from "../helpers/data"
+import Image from "react-bootstrap/Image";
+import Col from "react-bootstrap/Col";
 
 const Doctors = () => {
     /* console.log(doctorData); */
@@ -11,7 +13,7 @@ const Doctors = () => {
 
       
       
-      {
+     {/*  {
       doctorData.map((doctor)=> {
       const {id,name,dep,img } = doctor;
       return(
@@ -27,6 +29,22 @@ const Doctors = () => {
 
       );
     })
+    } */}
+
+
+    {
+    doctorData.map(({id,name,dep,img})=>{
+      return (
+        <Col  xs={6} sm={4} lg={3} key={id}>
+        <h1>{id}</h1>
+        <Image className="img-thumbnail doctor-img w-100" src={img} roundedCircle alt={name}/>
+        <h5>{name}</h5>
+        <p>{dep} </p>
+        </Col>
+      )
+    }
+  
+  )
     }
       
 
